2a302a1aa5e59fa45bf3afbb9090210fcaffef03
Raistyan <114503841+Raistyan@users.noreply.github.com>
2025-12-25 01:03:45 +0700

    Create Cutscene.lua

---
+++ Cutscene.lua
@@ -0,0 +1,39 @@
+-- ==============================
+-- 🚫 DISABLE CUTSCENE (LEGENDARY+)
+-- ==============================
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Net = require(
+    ReplicatedStorage
+        :WaitForChild("Packages")
+        :WaitForChild("Net")
+)
+
+local ReplicateCutscene = Net:RemoteEvent("ReplicateCutscene")
+local StopCutscene = Net:RemoteEvent("StopCutscene")
+
+local disableCutscene = false
+local cutsceneConnection
+
+local function EnableCutsceneSkip()
+    if cutsceneConnection then
+        cutsceneConnection:Disconnect()
+    end
+
+    cutsceneConnection = ReplicateCutscene.OnClientEvent:Connect(function(...)
+        -- langsung stop cutscene
+        StopCutscene:FireServer()
+    end)
+
+    print("🚫 Cutscene Disabled")
+end
+
+local function DisableCutsceneSkip()
+    if cutsceneConnection then
+        cutsceneConnection:Disconnect()
+        cutsceneConnection = nil
+    end
+
+    print("🎬 Cutscene Enabled")
+end

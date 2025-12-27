48a37fd0f911aabba8a723a07c3f4989f627b87f
Raistyan <114503841+Raistyan@users.noreply.github.com>
2025-12-28 03:14:14 +0700

    Update Dunhill.lua

--- Dunhill.lua
+++ Dunhill.lua
@@ -519,7 +519,7 @@ end)
         local TabContent = Instance.new("ScrollingFrame", Content)
         TabContent.Name = TabName .. "Content"
         TabContent.Size = UDim2.new(1, -160, 1, -65)
-        TabContent.Position = UDim2.new(0, 150, 0, 30)
+        TabContent.Position = UDim2.new(0, 150, 0, 35)
         TabContent.BackgroundColor3 = Theme.Background
         TabContent.BackgroundTransparency = 1
         TabContent.BorderSizePixel = 0
